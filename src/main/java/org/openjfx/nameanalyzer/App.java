2d15fd56317d57f7031d3515bfd55a8a6e90d422
float <yahyatayoyahya@gmail.com>
2024-05-26 05:01:37 +0100

    added icon to the window

--- src/main/java/org/openjfx/nameanalyzer/App.java
+++ src/main/java/org/openjfx/nameanalyzer/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         scene = new Scene(loadFXML("login"));
+        primaryStage.getIcons().add(new Image(String.valueOf(App.class.getResource("icon.jpg"))));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
